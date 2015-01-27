6ad2e10becfb01bf6600c668fc24bdad6718b6f5
urock24 <jrforbes24@gmail.com>
2015-01-27 16:36:19 -0700

    myWebSite Initial Commit for 15 files
    Working on this primarily for games to help the kids with learining

---
+++ learningGames.js
@@ -0,0 +1,177 @@
+/**
+ * @author John2013
+ */
+
+var spellingList = [];
+var yourName = null;
+
+
+
+
+/**
+* This checks to see if there is already a spelling list
+* if not it creates one, if there is ask to keep or to create a new one. 
+*/
+function createList() {
+	
+	if (spellingList.length === 0) {
+        //prompts for the list and then adds to the var entry
+        var entry = prompt("Please enter your spelling words, seperated by a comma. example(word,word,word): ");
+        // takes entry checks to see if it has something and creates array.
+        if (entry != null && entry != "") {
+            //creates the array
+            spellingList = entry.split(",");
+			
+        }
+        // let them know nothing entered and call the function again.
+        else {
+            alert("you didn't enter anything!");
+            createList();
+        }
+	
+	}
+	else {
+		// keep the same list
+		if (confirm("Is this your spelling list? " + spellingList)) {
+			alert("Okay, Let's Play!!");
+			}
+		// create a new list
+		else {
+			spellingList = [];
+			createList();
+		}
+			
+	}
+	populateSpellList();
+}
+
+/**
+* This will take spelling list array and populate the spelling list on screen.
+*
+*/
+
+function populateSpellList() {	
+	
+	// for loop should run through spelling list array and create list items in "listSpelling"
+	var i;
+	for (i = 0; i < spellingList.length; i++ ) {
+		
+		// create a new li
+		var newLI = document.createElement("li");
+		var indSpellingWord = spellingList[i];
+		
+		// grab the spelling list item 
+		var newContent = document.createTextNode(indSpellingWord);
+		
+		
+		// add the spelling list item to the li
+		newLI.appendChild(newContent);
+		
+		// get the unordered list and add the new li
+		var displaySpellList = document.getElementById("listSpelling");
+		alert(newLI);
+		
+		displaySpellList.appendChild(newLI);
+		
+		
+		
+	}
+	
+}
+
+/**
+* This will add a value to the yourName variable
+*
+*/
+
+function createName() {
+	
+	if(yourName === null){
+		// get there name and return it. 
+		yourName = prompt("Please enter your name: ");			
+	}
+	else if(!(confirm("Is your name "+ yourName+"?"))){
+		
+			yourName = prompt("Please enter your name: ");
+			
+		}
+	}
+
+/**
+* This will call the game function based on target.
+*
+*/
+
+function playGame(target) {	
+	"use strict";
+	// if statement to pick the game to run.
+	if(target === 'MissingLetters.html'){
+		missingLetters();
+	}
+	else if(target === 'MixedUpLetters.html'){
+		mixedUpLetters();
+	}
+	else{
+		spellItOut();
+	}	
+}
+
+/**
+*  This will run the missing letters game.
+*
+*/
+function missingLetters() {
+	// game code to create and play
+	
+
+}
+
+/**
+*  This will run the mixed up letters game.
+*
+*/
+function mixedUpLetters() {
+	// need code
+	
+}
+
+/**
+*  This will run the spell it out game.
+*
+*/
+function spellItOut() {
+	// need code
+	
+}
+
+
+function gameWindow(target) {
+	
+	// set the iframe html to the target html 
+	document.getElementById('game_frame').src = target;
+    // run the createName function
+	createName();
+    // run the createList function 
+	createList();
+	//play game
+	playGame(target);
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
