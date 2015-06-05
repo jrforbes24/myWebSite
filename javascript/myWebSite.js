3a942aa2098dc4760d4dbafa92fc84ef7fac60c8
urock24 <jrforbes24@gmail.com>
2015-06-04 18:06:21 -0600

    add code for gallery and lightbox

--- javascript/myWebSite.js
+++ javascript/myWebSite.js
@@ -5,8 +5,6 @@ function displayLocation(position) {
     var div = document.getElementById("location");
     div.innerHTML = "You are at latitude "+ latitude + ", longitude"+ longitude;
    }
-   
-
 
 
 function getMyLocation() {
@@ -18,6 +16,35 @@ function getMyLocation() {
     }
     
 }
+
+/*
+Creating a lightbox for the gallery page
+*/
+
+// first the variables for the html
+var $overlay = $('<div id="overlay"></div>');
+var $image = $('<img>');
+var $caption = $('<p></p>');
+
+// next add the html to the dom
+$overlay.append($image);
+$overlay.append($caption);
+$('body').append($overlay);
+
+// capture the click event on a link to an image and display the light
+// box
+
+$('#imageGallery a').click(function(event) {
+    event.preventDefault();
+    var imageLocation = $(this).attr('href');
+
+    TODO
+
+
+});
+
+
+
 
 window.onload = getMyLocation;
 
